4eb6b391ad07b3b410a389915e6d982420d12e1f
raizapaula93 <raizapaula93@gmail.com>
2022-04-04 13:48:18 -0300

    @raizapaula93[Modify Tema Controller]

--- src/main/java/org/generation/blogPessoal/controller/TemaController.java
+++ src/main/java/org/generation/blogPessoal/controller/TemaController.java
@@ -1,7 +1,6 @@
 package org.generation.blogPessoal.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -18,18 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-
-
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 
 
@@ -37,95 +25,49 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 @RestController
 @RequestMapping("/temas")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@Tag(name = "Configuração de Tema", description = "Controller de Tema")
 public class TemaController {
 
 	@Autowired
-	private TemaRepository repository;
-	
-	@Operation(summary = "Selecione os temas")
-	@ApiResponses(value = {
-			@ApiResponse(
-					responseCode = "200",
-					description = "Lista de temas",
-					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Tema.class))}
-					),
-			@ApiResponse(
-					responseCode = "204",
-					description = "Tema não encontrado",
-					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseStatusException.class))}
-					),
-			@ApiResponse(
-					responseCode = "500",
-					description = "Internal server error",
-					content = @Content
-					)
-	})
-	
-	
-	
-//	@GetMapping
-//	public ResponseEntity<List<Tema>> getAll(){
-//		return ResponseEntity.ok(repository.findAll());
-//	}
-	
+	private TemaRepository temaRepository;
+
 	@GetMapping
-	public ResponseEntity<List<Tema>> getAll(){
-		List<Tema> list = repository.findAll();
-		if (list.isEmpty()) {
-			return ResponseEntity.status(204).build();
-		} else {
-			return ResponseEntity.status(200).body(list);
-		}
+	public ResponseEntity<List<Tema>> getAll() {
+		return ResponseEntity.ok(temaRepository.findAll());
+
 	}
-		
-//	@GetMapping("/{id}")
-//	public ResponseEntity<Tema> getById(@PathVariable long id){
-//		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
-//				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
-	//}
-	
-	
-	
-	@Operation(summary = "Seleciona por ID")
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Tema> getById(@PathVariable(value = "id") Long id) {
-		return repository.findById(id)
-				.map(resp -> ResponseEntity.status(200).body(resp))
-				.orElseGet(() -> {
-					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não encontrado");
-				});
+	public ResponseEntity<Tema> getById(@PathVariable long id) {
+		return temaRepository.findById(id)
+		.map(resp -> ResponseEntity.ok(resp))
+		.orElse(ResponseEntity.notFound().build());
 	}
-	
-	@Operation(summary = "Seleciona por assunto")
+
 	@GetMapping("/descricao/{descricao}")
-	public ResponseEntity<List<Tema>> getByTitle(@PathVariable String descricao){
-		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
+	public ResponseEntity<List<Tema>> getByDescricao(@PathVariable String descricao) {
+		return ResponseEntity.ok(temaRepository.findAllByDescricaoContainingIgnoreCase(descricao));
 	}
-	
-	@Operation(summary = "Atualiza um tema")
+
 	@PostMapping
-	public ResponseEntity<Tema> post(@Valid @RequestBody Tema tema){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(repository.save(tema));
+	public ResponseEntity<Tema> postTema(@Valid @RequestBody Tema tema) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
 	}
 
 	@PutMapping
-	public ResponseEntity<Tema> put(@Valid @RequestBody Tema tema){
-		return repository.findById(tema.getId())
-				.map(resp -> ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema)))
-				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+	public ResponseEntity<Tema> putTema(@Valid @RequestBody Tema tema) {
+		return temaRepository.findById(tema.getId())
+			.map(resp -> ResponseEntity.status(HttpStatus.OK).body(temaRepository.save(tema)))
+			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 	}
 
-	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
-	public void delete(@PathVariable long id) {
-		Optional<Tema> tema = repository.findById(id);
-
-		if(tema.isEmpty())
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-
-		repository.deleteById(id);
+	public ResponseEntity<?> deleteTema(@PathVariable long id) {
+		return temaRepository.findById(id)
+			.map(resposta -> {
+				temaRepository.deleteById(id);
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+			})
+			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 
 }
